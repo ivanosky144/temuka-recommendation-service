b53ce8f8f18b115be252ffc7ad26dea91a17966e
ivanosky144 <ivanoskytan@gmail.com>
2024-12-26 14:54:37 +0700

    fix: update handler boilerplate

--- src/main/java/com/app/internal/handler/ContentInteractionHandler.java
+++ src/main/java/com/app/internal/handler/ContentInteractionHandler.java
@@ -1,7 +1,7 @@
 package com.app.internal.handler;
 
-import com.app.internal.model.ContentInteraction;
 import com.app.internal.model.Post;
+import com.app.internal.model.User;
 import com.app.internal.model.Community;
 import com.app.internal.queue.ContentInteractionQueue;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +13,7 @@ public class ContentInteractionHandler {
     @Autowired
     private ContentInteractionQueue contentInteractionQueue;
 
-    public void handleContentInteraction(ContentInteraction contentInteraction) {
-        System.out.println("Processing Content Interaction for User: ");
+    public void handleContentInteraction(User user, Post post, Community community) {
+        System.out.println("Processing Content Interaction for User: " + user.getId());
     }
 }
--- src/main/java/com/app/internal/handler/EducationActivityHandler.java
+++ src/main/java/com/app/internal/handler/EducationActivityHandler.java
@@ -1,11 +1,9 @@
 package com.app.internal.handler;
 
-import com.app.internal.model.MajorInteraction;
-import com.app.internal.model.UniversityInteraction;
-import com.app.internal.model.EducationActivity;
 import com.app.internal.model.University;
 import com.app.internal.model.Community;
 import com.app.internal.model.Major;
+import com.app.internal.model.User;
 import com.app.internal.queue.EducationActivityQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,7 +14,7 @@ public class EducationActivityHandler {
     @Autowired
     private EducationActivityQueue educationActivityQueue;
 
-    public void handleEducationActivity(EducationActivity educationActivity) {
+    public void handleEducationActivity(User user, University university, Major major) {
         System.out.println("Processing Education activity for User: ");
     }
 
